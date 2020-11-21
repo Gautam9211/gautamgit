5ddde71f72ff60fe5633b63b23f3caa947d6369a
Gautam9211 <58233275+Gautam9211@users.noreply.github.com>
2020-11-21 15:46:04 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const  app = express();
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 const path = require('path');
 const hbs = require('hbs');
 const staticPath = path.join(__dirname,'../publicdir');
