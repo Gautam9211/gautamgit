21b59612a82319ca0ba19c870a0c9bde9ab470e8
Gautam9211 <58233275+Gautam9211@users.noreply.github.com>
2020-11-21 16:03:33 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.set('views',viewPath);
 app.use(express.static(staticPath));
 hbs.registerPartials(partialPath);
 
-app.get('/index',(req,res)=> {
+app.get('/',(req,res)=> {
     res.render('index');
 });
 
